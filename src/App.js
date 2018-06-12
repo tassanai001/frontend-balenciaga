17e825ab93f91741fbaa87ac834e7b572ef40df1
tassanai <tassanai.yeeton@gmail.com>
2018-06-13 00:09:39 +0700

    Add: Apollo connect Backend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:9000/graphql',
+});
+
 class App extends Component {
+  componentDidMount() {
+    console.log('From App');
+    client
+      .query({
+        query: gql`
+          {
+            User(name: "asdas", surname: "asdas") {
+              name
+              surname
+            }
+          }
+        `,
+      })
+      .then(result => console.log('FUCK', result))
+      .catch(error => console.log('Error:--> ', error));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+          </p>
+        </div>
+      </ApolloProvider>
     );
   }
 }
